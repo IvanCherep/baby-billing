7726e16c47e1d6797168a7ac58212b7532e3b889
Ivan <53270836+IvanCherep@users.noreply.github.com>
2024-05-12 21:32:11 +0300

    Delete cdr-service directory

--- cdr-service/src/main/java/ru/nexignbootcamp/babybilling/cdrservice/mappers/Mapper.java
+++
@@ -1,9 +0,0 @@
-package ru.nexignbootcamp.babybilling.cdrservice.mappers;
-
-public interface Mapper<A, B> {
-
-    B mapTo(A a);
-
-    A mapFrom(B b);
-
-}
--- cdr-service/src/main/java/ru/nexignbootcamp/babybilling/cdrservice/repositories/CDRRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nexignbootcamp.babybilling.cdrservice.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.nexignbootcamp.babybilling.cdrservice.domain.entities.CDREntity;
-
-@Repository
-public interface CDRRepository extends CrudRepository<CDREntity, Long> {
-}
